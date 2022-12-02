cda373f24f2e6fe05a25dcfb333b2b99a1c754dc
Ciel <47144701+Shuaiouke@users.noreply.github.com>
2022-12-02 16:45:38 +0800

    Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,25 @@
 fun main() {
+    fun List<String>.toSnacks(): List<Int> {
+        var current = 0
+        return buildList {
+            for (i in this@toSnacks) {
+                if (i == "") {
+                    add(current)
+                    current = 0
+                } else {
+                    current += i.toInt()
+                }
+            }
+        }
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.toSnacks().max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.toSnacks().sortedDescending().take(3).sum()
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
